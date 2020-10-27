33cfa5fec323ffed56628bf8fed38a23f572def7
madik520 <ilyshik919@gmail.com>
2020-10-27 13:15:33 +0200

    Updates

---
+++ precache-manifest.a1cd03cae52b32b99799b3ee28935451.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6725931743b751ad7ae5fc99e27e898b",
+    "url": "/phone/index.html"
+  },
+  {
+    "revision": "162b496cab6899776c79",
+    "url": "/phone/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d175cf49ca1d7f0ff5e6",
+    "url": "/phone/static/css/main.73b2a41d.chunk.css"
+  },
+  {
+    "revision": "162b496cab6899776c79",
+    "url": "/phone/static/js/2.647875db.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/phone/static/js/2.647875db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d175cf49ca1d7f0ff5e6",
+    "url": "/phone/static/js/main.40256893.chunk.js"
+  },
+  {
+    "revision": "8cbf81d3dd4e5ed40afa",
+    "url": "/phone/static/js/runtime-main.060872b8.js"
+  },
+  {
+    "revision": "d63d9eff35aa057bab9c5041938d6d69",
+    "url": "/phone/static/media/product-1.d63d9eff.png"
+  },
+  {
+    "revision": "ad7ef891046b2a56aa41db25cbcf2a9b",
+    "url": "/phone/static/media/product-2.ad7ef891.png"
+  },
+  {
+    "revision": "d27ec2bc66073aa4b0b80368a9ac410e",
+    "url": "/phone/static/media/product-3.d27ec2bc.png"
+  },
+  {
+    "revision": "d9ed75372e686c2f8c38a53a52482e66",
+    "url": "/phone/static/media/product-4.d9ed7537.png"
+  },
+  {
+    "revision": "8f9a1f89508713ff90dc1b497c92b1b2",
+    "url": "/phone/static/media/product-5.8f9a1f89.png"
+  },
+  {
+    "revision": "b49d1b7cf1b82dad0a8059302706685a",
+    "url": "/phone/static/media/product-6.b49d1b7c.png"
+  },
+  {
+    "revision": "4c6ab223c32bec22d9cd6070caaedf19",
+    "url": "/phone/static/media/product-7.4c6ab223.png"
+  },
+  {
+    "revision": "d390282f89018d8777d36210b7df6d7b",
+    "url": "/phone/static/media/product-8.d390282f.png"
+  }
+]);
